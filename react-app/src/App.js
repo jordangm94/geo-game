2b2e44ff0633fd8f554a40b2d4f381d46c13008d
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-26 14:22:49 -0500

    Added and integrated Leaflet Maps API as a second map that users will be able to choose the location they are currently at

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
+import React, {useState, useRef} from 'react';
 import BingMapsReact from "bingmaps-react";
-import { useState } from 'react';
+
+import { MapContainer } from 'react-leaflet/MapContainer';
+import { TileLayer } from 'react-leaflet/TileLayer';
+import { useMap } from 'react-leaflet/hooks';
 
 function App() {
   const [isMapLoaded, setIsMapLoaded] = useState(false);
 
+  const mapRef = useRef()
+
+
+  
+
+
   return (
-    <div>
+    <>
+    <div ref={mapRef}>Hello World</div>
+    <MapContainer ref={mapRef} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+
+      
     <BingMapsReact
       onMapReady={() => {setIsMapLoaded(true)}}
       bingMapsKey="AvbDxkEoBBjb3rv1rfFWxUKJSLPGi8yLYCP9VC02-dgCUeDnDFg4-LCP4y16Nz-g"
@@ -21,7 +40,7 @@ function App() {
         streetsideOptions: { showExitButton: false, showCurrentAddress: false }
       }}
     />
-    </div>
+    </>
   );
 }
 
