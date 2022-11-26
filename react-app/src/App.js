b01486314a5c0f15770a35375f2195b725c1fe4b
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-26 14:01:34 -0300

    Add leaderboard page/route

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Leaderboard from "./pages/Leaderboard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/leaderboard" element={<Leaderboard />} />
     </Routes>
   );
 }
---
+++ react-app/src/pages/Leaderboard.js
@@ -0,0 +1,5 @@
+export default function Leaderboard() {
+  return (
+    <h1>This is the leaderboard page</h1>
+  )
+}
