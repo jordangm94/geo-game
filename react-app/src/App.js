47ef72c5fb98e3c69284128f398686da64cca440
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-26 13:40:42 -0300

    Create a Login page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
