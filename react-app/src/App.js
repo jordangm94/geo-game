ec61a0952e84f04129fca988e6a97ba0648bfde6
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-26 13:46:38 -0300

    Add register page/route

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
