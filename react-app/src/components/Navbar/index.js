6f36c147ae94133fb4e9313f9ac4da0746dda3e2
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-27 15:55:57 -0300

    Implement the index of Navbar and made the elements and import it:

--- react-app/src/components/Navbar/index.js
+++ react-app/src/components/Navbar/index.js
@@ -1,7 +1,33 @@
+import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from "./NavbarElements";
+
 export default function index() {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <>
+      <Nav>
+        <NavLink to="/">
+          <h1>Logo</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/game" activeStyle>
+            Play
+          </NavLink>
+          <NavLink to="/leaderboard" activeStyle>
+            Leaderboard
+          </NavLink>
+          <NavLink to="/help" activeStyle>
+            Help
+          </NavLink>
+          <NavLink to="/register" activeStyle>
+            Register
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to="/login">Log In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
   );
 };
+
+// Change h1 Logo to an image tag when we have a logo
