4c3223976319255e423ead3d7db5c70d3a145073
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-27 18:51:20 -0500

    Implemented video component for background of home page, temporary names being used just to ensure it works. Adjusting CSS.

---
+++ react-app/src/components/Home/Video.js
@@ -0,0 +1,10 @@
+import Home_Video_Pexels from './simple.mp4'
+
+export default function Video () {
+
+  return (
+    <>
+    <video src={Home_Video_Pexels} autoPlay loop muted/>
+    </>
+  )
+}
--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -1,7 +1,11 @@
 import TitleDescription from "../components/Home/TitleDescription";
+import Video from "../components/Home/Video";
 
 export default function Home() {
   return (
+    <>
     <TitleDescription />
+    <Video />
+    </>
   );
 }
