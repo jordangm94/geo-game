fc461897fa184d5b2d4600d7e61200091c1f94d7
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-27 13:29:06 -0500

    Created round/score or 'GameStatus' component to display round and score user has accumulated. Layered over map components. Added small temporary css styling

--- react-app/src/pages/Game.js
+++ react-app/src/pages/Game.js
@@ -1,10 +1,12 @@
 import AnswerMap from '../components/AnswerMap';
 import QuestionMap from '../components/QuestionMap';
+import GameStatus from '../components/GameStatus';
 
 export default function Game() {
 
   return (
     <>
+    <GameStatus />
     <AnswerMap />
     <QuestionMap />
     </>
