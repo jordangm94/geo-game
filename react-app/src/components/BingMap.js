4c41b4b3b1a38fe563511ca57c26f02cc02893c5
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-26 20:05:20 -0500

    Adjusted css and styling of components so that each map takes up half of the view port when playing the game

--- react-app/src/components/BingMap.js
+++ react-app/src/components/BingMap.js
@@ -1,25 +1,26 @@
 import React, {useState} from 'react';
 import BingMapsReact from "bingmaps-react";
 
-//This component houses the BingMap map, which show the player a random place on earth in which they will have to guess where they are
-
+//This component houses the Bing map, which shows the player a random place on earth in which they will have to guess where they are.
 export default function BingMap () {
   const [isMapLoaded, setIsMapLoaded] = useState(false);
 
   return (
+    <div class="BingMap" >
     <BingMapsReact
     onMapReady={() => {setIsMapLoaded(true)}}
     bingMapsKey="AvbDxkEoBBjb3rv1rfFWxUKJSLPGi8yLYCP9VC02-dgCUeDnDFg4-LCP4y16Nz-g"
-    height="1000px"
+    height="100vh"
     mapOptions={{
       navigationBarMode: "square",
     }}
-    width="1000px"
+    width="100vh"
     viewOptions={{
       center: { latitude: 42.360081, longitude: -71.058884 },
       mapTypeId: "streetside",
       streetsideOptions: { showExitButton: false, showCurrentAddress: false }
     }}
   />
+  </div>
   )
 }
--- react-app/src/components/LeafletMap.js
+++ react-app/src/components/LeafletMap.js
@@ -3,12 +3,12 @@ import { MapContainer } from 'react-leaflet/MapContainer';
 import { TileLayer } from 'react-leaflet/TileLayer';
 import { useMap } from 'react-leaflet/hooks';
 
-//This component houses the Leaflet map, which the player will use to naviagate the world and guess their location.
+//This component houses the Leaflet map, which the player will use to naviagate the world and input/guess their location with the click of the mouse.
 export default function LeafletMap() {
   const mapRef = useRef();
 
   return (
-    <MapContainer ref={mapRef} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer ref={mapRef} center={[51.505, -0.09]} zoom={3} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
