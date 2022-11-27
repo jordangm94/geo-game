988d8dda9552751fcc416d3b24e6e891074bcfc1
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-27 14:48:33 -0300

    Install styled-components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -9,6 +10,7 @@ import Leaderboard from "./pages/Leaderboard";
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
