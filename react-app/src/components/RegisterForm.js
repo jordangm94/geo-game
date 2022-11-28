9ae5bcb125705bb6da9dddbc665e1992552110b5
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-28 19:47:51 -0300

    Add validations

--- react-app/src/components/RegisterForm.js
+++ react-app/src/components/RegisterForm.js
@@ -4,20 +4,71 @@ import { Form, Button, Input } from "antd";
 
 export default function RegisterForm() {
   return (
-    <Form labelCol={{ span: 10 }} wrapperCol={{ span: 14 }}>
-      <Form.Item name="user_name" label="Username">
+    <Form autocomplete="off" labelCol={{ span: 10 }} wrapperCol={{ span: 14 }}>
+      <Form.Item name="user_name" label="Username" rules={[
+        {
+          required: true,
+          message: "Please enter a username"
+        },
+        {
+          whitespace: true,
+          message: "Please enter a username"
+        },
+        {
+          min: 6,
+          message: "Username must be at least 6 characters"
+        }
+      ]}
+        hasFeedback
+      >
         <Input placeholder="Type your username" />
       </Form.Item>
-      <Form.Item name="email" label="Email">
+      <Form.Item name="email" label="Email" rules={[
+        {
+          required: true,
+          message: "Please enter an email"
+        },
+        {
+          type: "email",
+          message: "Please enter a valid email"
+        }
+      ]}
+        hasFeedback
+      >
         <Input placeholder="Type your email" />
       </Form.Item>
-      <Form.Item name="password" label="Password">
+      <Form.Item name="password" label="Password" rules={[
+        {
+          required: true,
+          message: "Please enter a password"
+        },
+        {
+          min: 8,
+          message: "Password must be at least 8 characters"
+        }
+      ]}
+        hasFeedback
+      >
         <Input.Password placeholder="Type your password" />
       </Form.Item>
-      <Form.Item name="confirmPassword" label="Confirm Password">
+      <Form.Item name="confirmPassword" label="Confirm Password" rules={[
+        {
+          required: true,
+          message: "Please confirm your password"
+        },
+        ({ getFieldValue }) => ({
+          validator(_, value) {
+            if (!value || getFieldValue('password') === value) {
+              return Promise.resolve();
+            }
+            return Promise.reject("The password confirmation does not match");
+          }
+        })
+      ]}
+        hasFeedback>
         <Input.Password placeholder="Confirm your password" />
       </Form.Item>
-      <Form.Item>
+      <Form.Item wrapperCol={{ span: 24 }}>
         <Button type="primary" htmlType="submit">
           Register
         </Button>
