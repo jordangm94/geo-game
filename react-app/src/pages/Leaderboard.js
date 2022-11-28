145a3655b1bec5a2dcec8e569b325f1472685492
Kate Galich <kate.galich@outlook.com>
2022-11-27 19:00:54 -0500

    Fixing React warnings

--- react-app/src/pages/Leaderboard.js
+++ react-app/src/pages/Leaderboard.js
@@ -3,14 +3,18 @@ import { useState, useEffect } from "react";
 export default function Leaderboard() {
   const [state, setState] = useState([]);
 
-  useEffect(async () => {
-    let response = await fetch("http://localhost:8001/api/users/scores");
-    let newData = await response.json();
-    setState(newData);
+  useEffect(() => {
+    async function fetchData() {
+      let response = await fetch("http://localhost:8001/api/users/scores");
+      let newData = await response.json();
+      setState(newData);
+    }
+
+    fetchData();
   }, []);
 
-  let scoreElements = state.map(line =>
-    (<li>{line.user_name}: {line.total}</li>));
+  let scoreElements = state.map((line, index) =>
+    (<li key={index}>{line.user_name}: {line.total}</li>));
 
   return (
     <div>
