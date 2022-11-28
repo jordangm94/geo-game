9ba83afd5c00ce7128f92ec1e2ee8ab22348b224
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-28 14:39:46 -0500

    Deleted images folder and status component as no longer needed. Commented out bing map script tag as no longer needed also. Found out can simply use OverviewMapMode: 2 in order to disable map mode switch button

--- react-app/src/pages/Game.js
+++ react-app/src/pages/Game.js
@@ -1,22 +1,15 @@
 import AnswerMap from '../components/AnswerMap';
 import QuestionMap from '../components/QuestionMap';
 import GameStatus from '../components/GameStatus';
-import Status from '../components/Status';
 
 export default function Game() {
-  let Microsoft = window.Microsoft;
+  // let Microsoft = window.Microsoft;
 
   return (
     <main>
-      { Microsoft ?
-      <>
       <GameStatus />, 
       <QuestionMap />,
       <AnswerMap /> 
-      </>  
-      : 
-      null 
-    }    
     </main>
   );
 };
