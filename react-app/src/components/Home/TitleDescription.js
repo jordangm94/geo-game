4490d9f0bc6e202308f5e663a9f9dfced11ad0e9
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-27 20:08:55 -0500

    Adjusted styling and fixed overflow margin issue

--- react-app/src/components/Home/TitleDescription.js
+++ react-app/src/components/Home/TitleDescription.js
@@ -3,15 +3,18 @@ export default function TitleDescription() {
 
   return (
     <>
+   
       <div className="home-container">
         <div className="home-title">
-          Explore the world through
+          Explore the Globe!
         </div>
+        <div className="home-game-title-container">
         <h1 className="home-game-title">
           Globe Hunch
         </h1>
+        </div>
         <div className="home-description">
-          Explore your surroundings, learn about new locations, <br/>all while earning points and climbing the leaderboard.<br/>Free to play!
+          Survey incredible surroundings, learn about new locations, <br />all while earning points and climbing the leaderboard.<br />Free to play!
         </div>
       </div>
     </>
