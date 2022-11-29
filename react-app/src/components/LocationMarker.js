9b6c11819b37beeb6f760464718bc16c2f1efb8c
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-28 20:31:09 -0500

    Added personal notes to explain what is happening in component

--- react-app/src/components/LocationMarker.js
+++ react-app/src/components/LocationMarker.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import { Marker, Popup, useMapEvents } from 'react-leaflet';
 
+//This component will set a marker on the map wherever the user clicks
 export default function LocationMarker() {
+  //Set position state as null to begin
   const [position, setPosition] = useState(null)
   const map = useMapEvents({
+    //Once user click event happens, set position to the lat and long of where they clicked on map
     click(event) {
       setPosition(event.latlng)
       console.log(event.latlng)
     }
   })
-
+  //If position has been set by user, return marker component which will mark the map
   return position === null ? null : (
     <Marker position={position}>
       <Popup>You are here</Popup>
