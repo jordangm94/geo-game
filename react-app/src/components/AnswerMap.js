28f2dea93e7a1e3fbb67b82f80a12e4043f81302
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-28 20:28:35 -0500

    Created Location Marker component that will use state to set a position marker on where the user clicks on the leaflet/answer map

--- react-app/src/components/AnswerMap.js
+++ react-app/src/components/AnswerMap.js
@@ -1,14 +1,14 @@
 import React, { useState, useRef } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet';
 import "./AnswerMap.css";
-
+import LocationMarker from './LocationMarker';
 
 
 //This component houses the Leaflet map, which the player will use to naviagate the world and input/guess their location with the click of the mouse.
 export default function AnswerMap() {
   const mapRef = useRef();
 
-  const position = [51.505, -0.09];
+  
 
   return (
     <MapContainer ref={mapRef} center={[51.505, -0.09]} zoom={3} scrollWheelZoom={false}>
@@ -16,7 +16,7 @@ export default function AnswerMap() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position} />
+      <LocationMarker />
     </MapContainer>
   );
 }
