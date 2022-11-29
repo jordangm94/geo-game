44cb7770684c56aca0da1b72ae7557ee9b105d69
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-28 21:59:15 -0300

    Organize code

--- react-app/src/components/RegisterForm.js
+++ react-app/src/components/RegisterForm.js
@@ -4,67 +4,89 @@ import { Form, Button, Input } from "antd";
 
 export default function RegisterForm() {
   return (
-    <Form autocomplete="off" labelCol={{ span: 10 }} wrapperCol={{ span: 14 }}>
-      <Form.Item name="user_name" label="Username" rules={[
-        {
-          required: true,
-          message: "Please enter a username"
-        },
-        {
-          whitespace: true,
-          message: "Please enter a username"
-        },
-        {
-          min: 6,
-          message: "Username must be at least 6 characters"
-        }
-      ]}
+    <Form
+      autoComplete="off"
+      labelCol={{ span: 10 }}
+      wrapperCol={{ span: 14 }}
+      onFinish={(values) => {
+        console.log({ values });
+      }}
+      onFinishFailed={(error) => {
+        console.log(error);
+      }}
+    >
+      <Form.Item name="user_name"
+        label="Username"
+        rules={[
+          {
+            required: true,
+            message: "Please enter a username"
+          },
+          {
+            whitespace: true,
+            message: "Please enter a username"
+          },
+          {
+            min: 6,
+            message: "Username must be at least 6 characters"
+          }
+        ]}
         hasFeedback
       >
         <Input placeholder="Type your username" />
       </Form.Item>
-      <Form.Item name="email" label="Email" rules={[
-        {
-          required: true,
-          message: "Please enter an email"
-        },
-        {
-          type: "email",
-          message: "Please enter a valid email"
-        }
-      ]}
+      <Form.Item
+        name="email"
+        label="Email"
+        rules={[
+          {
+            required: true,
+            message: "Please enter an email"
+          },
+          {
+            type: "email",
+            message: "Please enter a valid email"
+          }
+        ]}
         hasFeedback
       >
         <Input placeholder="Type your email" />
       </Form.Item>
-      <Form.Item name="password" label="Password" rules={[
-        {
-          required: true,
-          message: "Please enter a password"
-        },
-        {
-          min: 8,
-          message: "Password must be at least 8 characters"
-        }
-      ]}
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: "Please enter a password"
+          },
+          {
+            min: 8,
+            message: "Password must be at least 8 characters"
+          }
+        ]}
         hasFeedback
       >
         <Input.Password placeholder="Type your password" />
       </Form.Item>
-      <Form.Item name="confirmPassword" label="Confirm Password" rules={[
-        {
-          required: true,
-          message: "Please confirm your password"
-        },
-        ({ getFieldValue }) => ({
-          validator(_, value) {
-            if (!value || getFieldValue('password') === value) {
-              return Promise.resolve();
+      <Form.Item
+        name="confirmPassword"
+        label="Confirm Password"
+        dependencies={['password']}
+        rules={[
+          {
+            required: true,
+            message: "Please confirm your password"
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject("The password confirmation does not match");
             }
-            return Promise.reject("The password confirmation does not match");
-          }
-        })
-      ]}
+          })
+        ]}
         hasFeedback>
         <Input.Password placeholder="Confirm your password" />
       </Form.Item>
@@ -73,6 +95,6 @@ export default function RegisterForm() {
           Register
         </Button>
       </Form.Item>
-    </Form>
+    </Form >
   );
 }
