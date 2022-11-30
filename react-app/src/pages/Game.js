8011b5a282cecb975a8c90a694a074c069192cff
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-30 12:30:50 -0500

    Created next turn function that will check the current state of the turn and move through array of turn objects accordingly

--- react-app/src/pages/Game.js
+++ react-app/src/pages/Game.js
@@ -10,35 +10,43 @@ export default function Game() {
 
   useEffect(() => {
     async function fetchData() {
-      //Use post response to create new game in DB, return object with information
+      //Use post request to create new game in DB, returns response object with information.
       let response = await fetch("http://localhost:8001/api/games/101", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' }
+        //Will need to add a Body: JSON stringify here for information we are sending which, will be userID
       });
       //Store new game information from DB in a variable
       let gameData = await response.json();
       setGame(gameData); //This will set state of game to value of gameData object from DB
       setTurn(gameData.turns[0]) //This will set the turn to the first first turn in the game
     }
-
+    
     fetchData();
   }, []);
+  console.log(game)
+  console.log("Hello from turn:", turn)
   
   //Create a function that increments through array of turn objects and sets state to new turn object each time answer button is clicked
-  const nextTurn = function() {
-    console.log(game)
-    // setTurn(game.turns)
-    console.log(turn)
-  };
-
-  return (
+  const nextTurn = function() {  
+    if (turn === game.turns[0]) {
+        setTurn(game.turns[1])
+      } 
+      if (turn === game.turns[1]) {
+        setTurn(game.turns[2])
+      } if (turn === game.turns[2]) {
+        console.log("Congratulations on completing the game")
+      }
+    };
+    
+    return (
     <main>
       {game && (
         <>
           <GameStatus />,
           <QuestionMap />,
           <AnswerMap />,
-          <Button gameData={game} onClick={nextTurn} className={"button-game-answer"} title={"Answer"} />
+          <Button  onClick={nextTurn} className={"button-game-answer"} title={"Answer"} />
         </>
       )}
     </main>
