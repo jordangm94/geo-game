0158e756914e80097a75ab0c9f28ea6d5917db30
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-29 22:47:48 -0500

    Added use Effect hook to create new game via post request to db once game page loaded

--- react-app/src/pages/Game.js
+++ react-app/src/pages/Game.js
@@ -1,10 +1,41 @@
+import { useState, useEffect } from 'react';
 import AnswerMap from '../components/AnswerMap';
 import QuestionMap from '../components/QuestionMap';
 import GameStatus from '../components/GameStatus';
 import Button from '../components/Button';
 
 export default function Game() {
-  // let Microsoft = window.Microsoft;
+  // const [state, setState] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await fetch("http://localhost:8001/api/games/101", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+      });
+      let newData = await response.json();
+      console.log(newData)
+      // setState(newData);
+    }
+
+    fetchData();
+  }, []);
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     let response = await fetch("http://localhost:8001/api/games/101", {
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' }
+  //     });
+  //     let newData = await response.json();
+  //     console.log(newData)
+  //     // setState(newData);
+  //   }
+
+  //   console.log(fetchData())
+  //   fetchData();
+  // }, []);
+
 
   return (
     <main>
