966afd5a9ea7b3456336c5800cdbff261ce1b467
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-29 23:04:38 -0500

    Applied conditional rendering to only show map tiles once game JSON object has been retrieved

--- react-app/src/pages/Game.js
+++ react-app/src/pages/Game.js
@@ -5,22 +5,25 @@ import GameStatus from '../components/GameStatus';
 import Button from '../components/Button';
 
 export default function Game() {
-  // const [state, setState] = useState([]);
+  const [game, setGame] = useState(null);
+  console.log(game)
 
   useEffect(() => {
     async function fetchData() {
+      //Use post response to create new game in DB, return object with information
       let response = await fetch("http://localhost:8001/api/games/101", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' }
       });
-      let newData = await response.json();
-      console.log(newData)
-      // setState(newData);
+      //Store new game information from DB in a variable
+      let gameData = await response.json();
+      //Set state of game to the response object
+      setGame(gameData)
     }
-
+    
     fetchData();
   }, []);
-
+  console.log(game)
   // useEffect(() => {
   //   async function fetchData() {
   //     let response = await fetch("http://localhost:8001/api/games/101", {
@@ -39,10 +42,14 @@ export default function Game() {
 
   return (
     <main>
+      {game && (
+        <>
       <GameStatus />, 
       <QuestionMap />,
-      <AnswerMap /> 
+      <AnswerMap />,
       <Button className={"button-game-answer"} title={"Answer"} />
+      </>
+      )}
     </main>
   );
 };
