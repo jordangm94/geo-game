dfdfd3875f534b08bd5def6cfd5987f229e5ecf0
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-12-02 15:03:11 -0300

    Refactor calculateScore function

--- server/routes/games.js
+++ server/routes/games.js
@@ -57,15 +57,15 @@ module.exports = db => {
   // Helper functions to calculate distance and score
   function calculateDistanceKm(questionLat, questionLon, answerLat, answerLon) {
 
-    var R = 6371.0710; 
-    var rlat1 = questionLat * (Math.PI/180); // Convert degrees to radians
-    var rlat2 = answerLat * (Math.PI/180); // Convert degrees to radians
-    var difflat = rlat2-rlat1; // Radian difference (latitudes)
-    var difflon = (answerLon-questionLon) * (Math.PI/180); // Radian difference (longitudes)
+    var R = 6371.0710;
+    var rlat1 = questionLat * (Math.PI / 180); // Convert degrees to radians
+    var rlat2 = answerLat * (Math.PI / 180); // Convert degrees to radians
+    var difflat = rlat2 - rlat1; // Radian difference (latitudes)
+    var difflon = (answerLon - questionLon) * (Math.PI / 180); // Radian difference (longitudes)
 
-    var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
+    var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat / 2) * Math.sin(difflat / 2) + Math.cos(rlat1) * Math.cos(rlat2) * Math.sin(difflon / 2) * Math.sin(difflon / 2)));
     return Math.round(d);
-  
+
 
     // const earthRadiusKm = 6371;
     // const radiansQuestionLat = questionLat * Math.PI / 180;
@@ -85,16 +85,14 @@ module.exports = db => {
 
   function calculateTurnScore(distanceKm) {
     const multiplier = 0.5;
-    let maxRoundScore = 5000;
 
-    maxRoundScore - (distanceKm * multiplier);
+    const roundScore = 5000 - (distanceKm * multiplier);
 
-    if (maxRoundScore < 0) {
+    if (roundScore < 0) {
       return 0;
     }
 
-
-    return maxRoundScore;
+    return roundScore;
   }
 
   // get user's games
@@ -315,7 +313,7 @@ module.exports = db => {
 
   router.put("/calculate/:turn_id", (req, res) => {
     const { questionLat, questionLon, answerLat, answerLon } = req.body;
-    console.log('Hello from req.body', req.body)
+    console.log('Hello from req.body', req.body);
 
     const distanceKm = calculateDistanceKm(questionLat, questionLon, answerLat, answerLon);
     console.log(distanceKm);
