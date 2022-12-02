3831e2467449ce0596c551c3767120e24ba15635
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-12-02 18:08:40 -0500

    Applied css changes and created video component to hold future tutorial video

--- react-app/src/pages/Help.js
+++ react-app/src/pages/Help.js
@@ -1,11 +1,13 @@
 import "./Help.css"
 import HelpTitleDescription from "../components/Help/HelpTitleDescription"
+import TutorialVideo from "../components/Help/TutorialVideo"
 
 export default function Help() {
 
   return (
     <main className="help-page-container">
       <HelpTitleDescription />
+      <TutorialVideo />
     </main>
   )
 }
