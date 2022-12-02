e4e7325d210059cb9d2885c3a4b2e9a83d00ca54
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-12-02 18:20:10 -0500

    Added brand font to Globe Hunch words in description

--- react-app/src/components/Help/HelpTitleDescription.js
+++ react-app/src/components/Help/HelpTitleDescription.js
@@ -3,7 +3,7 @@ export default function HelpTitleDescription() {
   return (
     <div className="help-title-description-container">
       <h1 className="help-title">Tutorial</h1>
-      <p className="help-description">Checkout this quick tutorial video and learn how to navigate your surroundings,<br></br> select a location and more while playing Globe Hunch! </p>
+      <p className="help-description">Checkout this quick tutorial video and learn how to navigate your surroundings,<br></br> select a location and more while playing <p style={{fontFamily: "Ultra", color: "#19191a"}}>Globe Hunch!</p></p>
     </div>
   )
 }
