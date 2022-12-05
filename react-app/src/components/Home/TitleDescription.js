228e8f266117c6e75933b777650d89ab36b3c298
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-12-05 15:59:59 -0500

    Moved button component into Titledescription container, and restyled CSS to keep button centered with title and description when screen size shrinks

--- react-app/src/components/Home/TitleDescription.js
+++ react-app/src/components/Home/TitleDescription.js
@@ -1,18 +1,29 @@
-//This comomponent holds the GlobeHunch brand text and description for the home page
+import Button from "../Button";
+import { useNavigate } from "react-router-dom";
+
+//This comomponent holds all contents appearing on top of video on home page: GlobeHunch title, description, and play now button
 
 export default function TitleDescription() {
+
+  const navigate = useNavigate();
+
+  const navigateToGame = function() {
+    console.log("Click function navigate to game");
+    navigate('/game');
+  };
 
   return (
     <>
       <div className="home-container">
         <div className="home-game-title-container">
-        <h1 className="home-game-title">
-          GlobeHunch
-        </h1>
+          <h1 className="home-game-title">
+            GlobeHunch
+          </h1>
         </div>
         <div className="home-description">
           Survey incredible surroundings, learn about new locations, <br />all while earning points and climbing the leaderboard.<br /><strong>Free to play!</strong>
         </div>
+        <Button className={"button-home-playnow"} title={"Play Now"} onClick={navigateToGame} />
       </div>
     </>
   );
