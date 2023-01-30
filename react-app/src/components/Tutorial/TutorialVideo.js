d79fa9ee9061bc16898b826b26a2e762a0ce3f24
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-01-30 17:52:26 -0500

    Corrected bug by using JSX to put true in { }.

--- react-app/src/components/Tutorial/TutorialVideo.js
+++ react-app/src/components/Tutorial/TutorialVideo.js
@@ -3,7 +3,7 @@ export default function Video () {
 
   return (
     <>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/XQQf1xKwkxY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="true"></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/XQQf1xKwkxY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
     </>
   )
 }
